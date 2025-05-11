021f064cf7efcad5150844b40547c5288703e29d
ankushbanotra <2302031000001@silveroakuni.ac.in>
2025-05-12 00:08:05 +0530

    Delete Online Store directory

--- Online Store/schemas/users.py
+++
@@ -1,9 +0,0 @@
-from marsh import ma
-from model.users import UserModel
-
-class UserSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = UserModel
-        load_only = ("password",)
-        dump_only = ("id","activated")
-        load_instance = True
--- Online Store/stripe_pay.py
+++
@@ -1,5 +0,0 @@
-import stripe,os
-from dotenv import load_dotenv
-load_dotenv() 
-
-stripe.api_key =  os.getenv("STRIPE_API")
